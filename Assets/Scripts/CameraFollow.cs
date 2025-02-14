c3a5071dd9338e2737898f31e4fc30788a5f8bcf
laghamaly <aghamaly@usc.edu>
2025-02-14 15:54:11 -0800

    Switching between characters and directing camera towards the enemy

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,19 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
-{
+public class Camera : MonoBehaviour {
+
     // Start is called before the first frame update
-    public GameObject player;
+    public float offsetDistance;
+    public int currentCharacterIdx;
+    public GameObject[] characters;
+    public GameObject enemy; 
     public Vector3 offset;
-    void Start()
-    {
-        
+
+    void Start() {
+        currentCharacterIdx = 0;
+        MoveToCharacter(currentCharacterIdx);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        transform.position = player.transform.position + offset;
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            currentCharacterIdx = (currentCharacterIdx + 1) % characters.Length;
+            MoveToCharacter(currentCharacterIdx);
+        }
+    }
+
+    void MoveToCharacter(int characterIdx) {
+        transform.position = characters[currentCharacterIdx].transform.position + offset;
+        transform.LookAt(enemy.transform.position);
+        transform.position -= transform.forward * offsetDistance;
     }
 }
